ea96de8f9a562c609b3572ace10ee1e742ad69e2
92Mohammad <m.e89571828@gmail.com>
2025-09-28 19:43:04 +0530

    feat: added create new task, update task , delete task feature & added sortable & draggable functionality

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,29 +1,71 @@
-import { GripVertical } from "lucide-react";
-import { useDraggable } from "@dnd-kit/core";
+import { useState } from "react";
+import { GripVertical, Trash2 } from "lucide-react";
 import { CSS } from "@dnd-kit/utilities";
+import type { Id } from "../types";
+import { useSortable } from "@dnd-kit/sortable";
+
 interface TaskCardProps {
-	id: string;
+	id: Id
 	title: string;
+	deleteTask: (id: Id) => void;
+	updateTask: (id: Id, newTitle: string) => void;
 }
 
-export function TaskCard({ id, title }: TaskCardProps) {
-	const { attributes, listeners, setNodeRef, transform } = useDraggable({
+export function TaskCard({ id, title, deleteTask, updateTask }: TaskCardProps) {
+	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
 		id,
+		data: {
+			type: 'Task'
+		}
 	});
+
 	const style = {
 		transform: CSS.Translate.toString(transform),
+		transition
 	};
+	const [editMode, setEditMode] = useState<boolean>(false);
+	const [updatedTitle, setUpdatedTitle] = useState<string>(title);
 
 	return (
 		<div
-			className="flex items-center  px-2.5 py-2 gap-3 rounded-xl border border-gray-200 bg-gray-200/50 cursor-move w-full "
+			className="flex items-center  px-2.5 py-2 gap-3 rounded-xl border border-gray-200 bg-gray-200/50 cursor-grab w-full justify-between "
 			style={style}
 			ref={setNodeRef}
 			{...listeners}
 			{...attributes}
 		>
-			<GripVertical className="w-4 h-4 text-gray-400 cursor-move" />
-			<span className="text-black font-normal">{title}</span>
+			<div className="flex flex-row gap-2 items-center ">
+				<GripVertical className="w-4 h-4 text-gray-400 cursor-move" />
+				{editMode ? (
+					<input
+						type="text"
+						autoFocus
+						value={updatedTitle}
+						placeholder="Edit task..."
+						className="outline-none border border-rose-300 rounded-lg px-2 text-sm py-1"
+						onChange={(e) => setUpdatedTitle(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								updateTask(id,  updatedTitle);
+								setEditMode(false)
+							}
+						}}
+					/>
+				) : (
+					<span
+						className="text-black font-normal"
+						onDoubleClick={() => setEditMode(true)}
+					>
+						{title}
+					</span>
+				)}
+			</div>
+			<button
+				className="bg-red-500/20 p-1 rounded-xl cursor-pointer"
+				onClick={() => deleteTask(id)}
+			>
+				<Trash2 className="w-4 h-4  text-red-500" />
+			</button>
 		</div>
 	);
 }
--- src/components/task-container.tsx
+++ src/components/task-container.tsx
@@ -1,34 +1,44 @@
-import type { Task } from "../types";
+import type { Id, Task } from "../types";
 import { TaskCard } from "./task-card";
-import { useDroppable } from "@dnd-kit/core";
+import { SortableContext, useSortable } from "@dnd-kit/sortable";
+
 interface TaskContainerProps {
-	id: string;
+	id: Id;
 	title: string;
 	tasks: Task[];
+	deleteTask: (id: Id) => void
+	onUpdateTask: (id: Id, newTitle: string) => void
 }
 
-export function TaskContainer({ id, title, tasks }: TaskContainerProps) {
-	// make it dropable
-
-	const { setNodeRef } = useDroppable({ id });
+export function TaskContainer({ id, title, tasks, deleteTask, onUpdateTask }: TaskContainerProps) {
+	const { setNodeRef } = useSortable({ 
+		id,
+		data: {
+			type: "Column"
+		}
+	});
 
 	return (
 		<div
 			ref={setNodeRef}
-			className="flex flex-col items-center rounded-xl border border-gray-200 px-4 py-2 min-w-48"
+			className="flex flex-col items-center rounded-xl border border-gray-200 px-4 py-2 min-w-48 shadow-sm"
 		>
 			<h2 className="text-xl font-semibold mb-5">{title}</h2>
-			<div className="flex flex-col gap-2  w-full">
-				{tasks.map((task) => {
-					return (
-						<TaskCard
-							key={task.id}
-							id={task.id}
-							title={task.title}
-						/>
-					);
-				})}
-			</div>
+			<SortableContext items={tasks.map(task => task.id)}>
+				<div className="flex flex-col gap-2  w-full">
+					{tasks.map((task) => {
+						return (
+							<TaskCard
+								key={task.id}
+								id={task.id}
+								title={task.title}
+								deleteTask={deleteTask}
+								updateTask = {onUpdateTask}
+							/>
+						);
+					})}
+				</div>
+			</SortableContext>
 		</div>
 	);
 }
